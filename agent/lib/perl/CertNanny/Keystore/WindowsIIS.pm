c9ce7fd84cc29eac2591c7347fd7ba4cd81b6d49
soerenrinne <soerenrinne@3c32a567-6419-0410-803f-b4c1ae207348>
2008-06-11 11:45:24 +0000

    added WindowsIIS Keystore updated config file updated documentation
    git-svn-id: https://certnanny.svn.sourceforge.net/svnroot/certnanny/trunk@62 3c32a567-6419-0410-803f-b4c1ae207348

---
+++ agent/lib/perl/CertNanny/Keystore/WindowsIIS.pm
@@ -0,0 +1,142 @@
+#
+# CertNanny - Automatic renewal for X509v3 certificates using SCEP
+# 2005, 2006 Martin Bartosch <m.bartosch@cynops.de>
+#
+# This software is distributed under the GNU General Public License - see the
+# accompanying LICENSE file for more details.
+#
+
+package CertNanny::Keystore::WindowsIIS;
+
+use base qw(Exporter CertNanny::Keystore::Windows CertNanny::Keystore::OpenSSL);
+
+use strict;
+use vars qw($VERSION);
+use Exporter;
+use Carp;
+use English;
+use Data::Dumper;
+use Win32::OLE;
+use Win32::OLE::Variant;
+use Win32::OLE::Const;
+
+$VERSION = 0.10;
+
+
+
+
+# This method is called once the new certificate has been received from
+# the SCEP server. Its responsibility is to create a new keystore containing
+# the new key, certificate, CA certificate keychain and collection of Root
+# certificates configured for CertNanny. The new certificate will be imported
+# in the IIS (with given InstanceName from the config), to avoid the loss of
+# secure bindings after replacing the certificate in the windows keystore.
+# A true return code indicates that the keystore was installed properly and
+# the certficate in the IIS has been successfully imported.
+sub installcert {
+    my $self = shift;
+    my %args = ( 
+		@_,         # argument pair list
+	);
+    
+    # create prototype PKCS#12 file
+    my $keyfile = $self->{STATE}->{DATA}->{RENEWAL}->{REQUEST}->{KEYFILE};
+    my $certfile = $args{CERTFILE}; 
+    my $label = $self->{CERT}->{LABEL};
+    
+    $self->info("Creating prototype PKCS#12 from certfile $certfile, keyfile $keyfile, label $label");
+
+	# create random PW via OpenSSL
+	my $openssl = $self->{OPTIONS}->{CONFIG}->get('cmd.openssl', 'FILE');
+   
+    my $open_result = open(my $OPENSSL, "\"$openssl\" rand -base64 15 |");
+	if (! $open_result) {
+		$self->seterror("Could not open OpenSSL for random PIN generation");
+		return;
+    }    
+    
+    # the OpenSSL output is saved in $randpin
+    my $randpin = do {
+		local $/;
+		<$OPENSSL>;
+	};
+    chomp($randpin);
+    close($OPENSSL);
+	
+	if (! $randpin) {
+		$self->seterror("No random PIN generated");
+	}
+	
+	$self->{PIN} = $randpin;
+
+    # pkcs12file must be an absolute filename (see below, gsk6cmd bug)
+    my $pkcs12file = $self->createpkcs12(
+	  FILENAME     => $self->gettmpfile(),
+	  EXPORTPIN    => $self->{PIN},
+	  CACHAIN      => undef,
+	);
+    
+    if (! defined $pkcs12file) {
+	    $self->seterror("Could not create prototype PKCS#12 from received certificate");
+	    return;
+    }
+    $self->info("Created prototype PKCS#12 file $pkcs12file");
+
+    # initialize IIS.CertObj
+	my $certobj = Win32::OLE->new ('IIS.CertObj');
+	if (! defined $certobj) {
+	    $self->seterror("Could not create IIS.CertObj");
+	    return;
+    }
+	
+	# read IIS Webserver InstanceName from config
+	my $instanceidentifier = $self->{OPTIONS}->{ENTRY}->{instanceidentifier};
+	$certobj->SetProperty('InstanceName', 'w3svc/' . $instanceidentifier);
+	$self->info("Using InstanceName w3svc/$instanceidentifier");
+	
+	my $result = $self->deleteoldcerts();
+
+    if($result == 1) {
+		# import the new certificate into IIS
+		$certobj->Import($pkcs12file, $self->{PIN}, 1, 1);
+	}
+	
+	# delete requests
+	my $store = $self->openstore('REQUEST', 'machine');
+	my $certs = $store->Certificates;
+
+    #go through all certificates in the store
+    my $enum = Win32::OLE::Enum->new($certs);
+    while (defined( my $cert = $enum->Next)) {
+		my $subjectname=$cert->SubjectName;
+		#Because the subject names in the certificates from the certificate store are formated in a different way
+		#the subject names from the config file. The blanks after the seperating "," need to be deleted.
+		$subjectname =~ s/(?<!\\)((\\\\)*),\s*/$1,/g;
+		if($subjectname eq $self->{OPTIONS}->{ENTRY}->{location}) {
+			# delete matching requests
+			$store->Remove($cert);
+		}	
+	}
+
+	# check if cert is installed (one match on name - getcertobject())
+	my $old_thumbprint = $self->{CERT}->{INFO}->{CertificateFingerprint};
+    $old_thumbprint =~ s/://g;
+
+	$self->info("Thumbprint of old certificate: $old_thumbprint");
+	
+	my $new_cert = $self->getcertobject( $self->{STORE} );
+	
+	my $new_thumbprint = $new_cert->thumbprint();
+	
+	$self->info("Thumbprint of new certificate: $new_thumbprint");
+	
+	if ($old_thumbprint eq $new_thumbprint) {
+		$self->seterror("Installation failed, old certificate is still in place.");
+		return;
+	}
+	
+    # only on success:
+    return 1;
+}
+
+1;
