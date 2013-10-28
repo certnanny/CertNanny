59b6b2f8b91239feb6706782ad149d731eb91f5e
Peter Kempf <pk@ekse.de>
2013-10-28 14:56:31 +0100

    * Test on defined Hash Element

--- agent/lib/perl/CertNanny/Keystore/OpenSSL.pm
+++ agent/lib/perl/CertNanny/Keystore/OpenSSL.pm
@@ -1338,10 +1338,10 @@ sub installRoots {
                                            SRCFILE => $cert->{CERTFILE}, 
                                            APPEND  => 1);
 
-                $self->{hook}->{Type}   .= 'CHAINFILE' . ','                                 if ($self->{hook}->{Type}   !~ m/CHAINFILE/);
-                $self->{hook}->{File}   .= $cert->{CERTFILE} . ','                           if ($self->{hook}->{File}   !~ m/$cert->{CERTFILE}/);
-                $self->{hook}->{FP}     .= $cert->{CERTINFO}->{CertificateFingerprint} . ',' if ($self->{hook}->{FP}     !~ m/$cert->{CERTINFO}->{CertificateFingerprint}/);
-                $self->{hook}->{Target} .= $locInstall{chainfile} . ','                      if ($self->{hook}->{Target} !~ m/$locInstall{chainfile}/);
+                $self->{hook}->{Type}   .= 'CHAINFILE' . ','                                 if (defined($self->{hook}->{Type})   && ($self->{hook}->{Type}   !~ m/CHAINFILE/));
+                $self->{hook}->{File}   .= $cert->{CERTFILE} . ','                           if (defined($self->{hook}->{File})   && ($self->{hook}->{File}   !~ m/$cert->{CERTFILE}/));
+                $self->{hook}->{FP}     .= $cert->{CERTINFO}->{CertificateFingerprint} . ',' if (defined($self->{hook}->{FP})     && ($self->{hook}->{FP}     !~ m/$cert->{CERTINFO}->{CertificateFingerprint}/));
+                $self->{hook}->{Target} .= $locInstall{chainfile} . ','                      if (defined($self->{hook}->{Target}) && ($self->{hook}->{Target} !~ m/$locInstall{chainfile}/));
               }    
               # Target chainfile part is finished. now write Target
               # put tmp-file to the right location     
