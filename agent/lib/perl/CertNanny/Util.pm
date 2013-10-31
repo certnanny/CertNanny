942305fb1a205acb51b6af41974bb457f353d34f
Peter Kempf <pk@ekse.de>
2013-10-31 13:43:08 +0100

    * Failure: Missing certformat parameter in getCertInfo

--- agent/lib/perl/CertNanny/Util.pm
+++ agent/lib/perl/CertNanny/Util.pm
@@ -450,11 +450,12 @@ sub callOpenSSL {
 
   my $rc = 0;
   my $info;
+
   # build commandstring
   my $openssl = $self->{CONFIG}->get('cmd.openssl', 'FILE');
   my @cmd = (qq("$openssl"), $command);
   push(@cmd, ('-in', qq("$args{CERTFILE}")))       if (defined $args{CERTFILE});
-  push(@cmd, ('-inform', qq("$args{CERTFORMAT}"))) if (defined $args{CERTFILE});
+  push(@cmd, ('-inform', qq("$args{CERTFORMAT}"))) if (defined $args{CERTFORMAT});
   foreach (@$params) {
     push(@cmd, -$_);
   }
