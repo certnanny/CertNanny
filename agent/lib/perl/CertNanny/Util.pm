cd4cb0181822b7a044da829138079497815f45e8
pgk69 <pk@ekse.de>
2013-11-11 17:46:27 +0100

    * JKS getKey rework

--- agent/lib/perl/CertNanny/Util.pm
+++ agent/lib/perl/CertNanny/Util.pm
@@ -101,7 +101,7 @@ sub hidePin() {
   my @cmd = @_;
   
   for (my $ii = 0; $ii < $#cmd; $ii++) {
-    $cmd[$ii + 1] = "*HIDDEN*" if ($cmd[$ii] =~ /(-pw|-target_pw|-storepass|-keypass)/);
+    $cmd[$ii + 1] = "*HIDDEN*" if ($cmd[$ii] =~ /(-pw|-target_pw|-storepass|-keypass|-srcstorepass|-deststorepass|-srckeypass|-destkeypass)/);
     $cmd[$ii] =~ s/Login=\S+/Login=*HIDDEN*/;
   }
   my $commando = join(' ', @cmd);
