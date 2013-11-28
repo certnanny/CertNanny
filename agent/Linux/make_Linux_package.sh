d4de5b743e6cd31b4bf50de0e241b7c7166720bd
Andreas Leibl <andreas@leibl.co.uk>
2013-11-28 23:48:23 +0000

    Fixed problem with Linux packaging and added template configs

--- agent/Linux/make_Linux_package.sh
+++ agent/Linux/make_Linux_package.sh
@@ -5,6 +5,6 @@ echo "Creating package..."
 #ts=$(date +'%Y%m%d%H%M%S')
 version=$(head -n 1 VERSION)
 sed "s/VERSIONINFO/$version/" < Linux/certnanny.spec.in > Linux/certnanny.spec
-tar --transform "s/^\./CertNanny-$version/" --exclude '.git' -czf $HOME/rpmbuild/SOURCES/CertNanny-$version.tar.gz .
+tar --transform "s/^\./certnanny-$version/" --exclude '.git' -czf $HOME/rpmbuild/SOURCES/certnanny-$version.tar.gz .
 rpmbuild -bb Linux/certnanny.spec
 
